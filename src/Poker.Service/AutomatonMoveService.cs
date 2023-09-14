3b245c705e02673e0acc9a17806ab244fad92705
Ben Osborne <osborne.ben@gmail.com>
2023-09-14 08:48:05 -0500

    removed use of DeepClone for Players

--- src/Poker.Service/AutomatonMoveService.cs
+++ src/Poker.Service/AutomatonMoveService.cs
@@ -22,13 +22,12 @@ public class AutomatonMoveService : IAutomatonMoveService
 
     private Task<MoveResult> DefaultMoveAsync(MoveArgs args)
     {
-        var playerOut = args.PlayerInTurn.DeepClone();
         uint potOut = args.Pot;
 
         return Task.FromResult(
             new MoveResult
             {
-                PlayerInTurn = playerOut,
+                PlayerInTurn = args.PlayerInTurn,
                 Deck = args.RoundArgs.Game.Deck,
                 Pot = potOut
             }
@@ -37,18 +36,24 @@ public class AutomatonMoveService : IAutomatonMoveService
 
     private Task<MoveResult> AnteAsync(MoveArgs args)
     {
-        var playerOut = args.PlayerInTurn.DeepClone();
+        var stackOut = args.PlayerInTurn.Participant.Stack;
+
         var ante = args.RoundArgs.Game.Ante;
 
         _userInterfaceService
-            .WriteLines($"{playerOut.Participant.Name} antes.");
+            .WriteLines($"{args.PlayerInTurn.Participant.Name} antes.");
 
-        playerOut.Participant.Stack -= ante;
+        stackOut -= ante;
 
         return Task.FromResult(
             new MoveResult
             {
-                PlayerInTurn = playerOut,
+                PlayerInTurn = args.PlayerInTurn with
+                {
+                    Participant = args.PlayerInTurn.Participant with {
+                        Stack = stackOut
+                    }
+                },
                 Deck = args.RoundArgs.Game.Deck,
                 Pot = args.Pot + ante
             }
@@ -57,14 +62,16 @@ public class AutomatonMoveService : IAutomatonMoveService
 
     private Task<MoveResult> DealAsync(MoveArgs args)
     {
-        var playerOut = args.PlayerInTurn.DeepClone();
+        var playerCardsOut = args.PlayerInTurn.Cards;
+
         var deckOut = args.RoundArgs.Game.Deck.DeepClone();
 
         var dealtCards = deckOut
             .Cards
             .Take(args.RoundArgs.Phase.GetCountOfCardsToDeal());
 
-        playerOut.Cards.AddRange(dealtCards);
+        playerCardsOut.AddRange(dealtCards);
+
         deckOut.Cards.RemoveAll(
             c => dealtCards
                 .Select(x => x.Id)
@@ -74,7 +81,7 @@ public class AutomatonMoveService : IAutomatonMoveService
         return Task.FromResult(
             new MoveResult
             {
-                PlayerInTurn = playerOut,
+                PlayerInTurn = args.PlayerInTurn with { Cards = playerCardsOut },
                 Deck = deckOut,
                 Pot = args.Pot
             }
