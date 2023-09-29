90a5d20bcf7c2402dc2ae6174e5365d0e9dce6bb
Ben Osborne <osborne.ben@gmail.com>
2023-09-28 22:03:26 -0500

    added Qualify_Returns_True_When_Hand_Contains_Flush_With_Two_Jokers

--- src/Poker.Domain.Tests/Implementations/HandQualifier/FlushTests.cs
+++ src/Poker.Domain.Tests/Implementations/HandQualifier/FlushTests.cs
@@ -66,7 +66,38 @@ public class FlushTests
     }
 
     [Fact]
-    public void Qualify_Returns_False_When_Hand_Does_Not_Contain_Flushd()
+    public void Qualify_Returns_True_When_Hand_Contains_Flush_With_Two_Jokers()
+    {
+        // arrange
+        var fixture = new HandQualifierTestFixture(
+            new()
+            {
+                ExpectedHandQualification = HandQualifications.Qualifies,
+                RemainingCards = 0,
+                Hand = Hands.Flush
+            })
+            .ExpectedInHand(x =>
+            {
+                x.With(
+                    new List<Card>() {
+                        Cards.AceOfHearts,
+                        Cards.CreateJoker() with { Impersonating = Cards.QueenOfHearts },
+                        Cards.FiveOfHearts,
+                        Cards.ThreeOfHearts,
+                        Cards.CreateJoker() with { Impersonating = Cards.KingOfHearts }
+                    }
+                );
+            });
+
+        // act
+        var result = fixture.Execute();
+
+        // assert
+        result.ShouldBeAsExpected();
+    }
+
+    [Fact]
+    public void Qualify_Returns_False_When_Hand_Does_Not_Contain_Flush()
     {
         // arrange
         var fixture = new HandQualifierTestFixture(
