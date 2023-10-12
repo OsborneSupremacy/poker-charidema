003b5333383529f7bf5cd2d43ec820411fb32c04
Ben Osborne <osborne.ben@gmail.com>
2023-10-12 12:29:34 -0500

    added Qualify_False_When_No_FullHouse_Exists

--- src/Poker.Domain.Tests/Implementations/HandQualifier/FullHouseTests.cs
+++ src/Poker.Domain.Tests/Implementations/HandQualifier/FullHouseTests.cs
@@ -49,7 +49,52 @@ public class FullHouseTests
                     }
                 );
             })
-            .ExpectedNeededCards(Cards.All.WhereRank(Ranks.Five));
+            .ExpectedNeededCards(
+                Cards.FourOfSpades,
+                Cards.FourOfClubs,
+                Cards.FourOfHearts,
+                Cards.FiveOfSpades,
+                Cards.FiveOfClubs,
+                Cards.FiveOfHearts
+            );
+
+        // act
+        var result = fixture.Execute();
+
+        // assert
+        result.ShouldBeAsExpected();
+    }
+
+    [Fact]
+    public void Qualify_False_When_No_ThreeOfAKind_Or_Pair_Exists()
+    {
+        var fixture = new HandQualifierTestFixture()
+            .For(Hands.FullHouse, HandQualifications.Eliminated)
+            .ExpectedInDeadCards(x =>
+            {
+                x.With(
+                    new List<Card>() {
+                        Cards.TwoOfHearts,
+                        Cards.TwoOfDiamonds,
+                        Cards.ThreeOfClubs,
+                        Cards.FourOfDiamonds,
+                        Cards.FiveOfDiamonds
+                    }
+                );
+             })
+            .ExpectedNeededCards(
+                Cards.TwoOfClubs,
+                Cards.TwoOfSpades,
+                Cards.ThreeOfSpades,
+                Cards.ThreeOfHearts,
+                Cards.ThreeOfDiamonds,
+                Cards.FourOfClubs,
+                Cards.FourOfSpades,
+                Cards.FourOfHearts,
+                Cards.FiveOfClubs,
+                Cards.FiveOfSpades,
+                Cards.FiveOfHearts
+            );
 
         // act
         var result = fixture.Execute();
