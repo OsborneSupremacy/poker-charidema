de25c0639717ad0f10299490b05596d9a6c830f9
Ben Osborne <osborne.ben@gmail.com>
2023-10-13 08:56:05 -0500

    Created "NoHand" template

---
+++ src/Poker.Domain/Classic/HandTemplates/NoHand.cs
@@ -0,0 +1,13 @@
+﻿namespace Poker.Domain.Classic;
+
+public partial record HandTemplates
+{
+    public static HandTemplate NoHand { get; } = new HandTemplate
+    {
+        Name = "No Hand",
+        Hand = Hands.NoHand,
+        HighRank = Ranks.Empty,
+        Suit = Suits.Empty,
+        CardGroups = new()
+    };
+}
