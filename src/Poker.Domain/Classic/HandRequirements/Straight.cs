72c84e8f4b9d02a8c44bd5b841b6a00bd49b6dff
Ben Osborne <osborne.ben@gmail.com>
2023-10-12 22:26:06 -0500

    Added Straight

---
+++ src/Poker.Domain/Classic/HandRequirements/Straight.cs
@@ -0,0 +1,341 @@
+﻿namespace Poker.Domain.Classic.HandRequirements;
+
+public record Straight
+{
+    public static HandRequirement SixHigh { get; } = new HandRequirement
+    {
+        Name = "Six High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Six,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Six).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Five).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Four).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Three).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Two).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement SevenHigh { get; } = new HandRequirement
+    {
+        Name = "Seven High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Seven,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Seven).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Six).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Five).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Four).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Three).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement EightHigh { get; } = new HandRequirement
+    {
+        Name = "Eight High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Eight,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Eight).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Seven).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Six).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Five).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Four).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement NineHigh { get; } = new HandRequirement
+    {
+        Name = "Nine High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Nine,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Nine).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Eight).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Seven).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Six).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Five).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement TenHigh { get; } = new HandRequirement
+    {
+        Name = "Ten High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Ten,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Ten).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Nine).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Eight).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Seven).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Six).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement JackHigh { get; } = new HandRequirement
+    {
+        Name = "Jack High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Jack,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Jack).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Ten).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Nine).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Eight).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Seven).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement QueenHigh { get; } = new HandRequirement
+    {
+        Name = "Queen High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Queen,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Queen).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Jack).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Ten).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Nine).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Eight).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement KingHigh { get; } = new HandRequirement
+    {
+        Name = "King High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.King,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.King).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Queen).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Jack).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Ten).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Nine).ToList()
+            }
+        }
+    };
+
+    public static HandRequirement AceHigh { get; } = new HandRequirement
+    {
+        Name = "Ace High Straight",
+        Hand = Hands.Straight,
+        HighRank = Ranks.Ace,
+        Suit = Suits.Empty,
+        CardGroups = new List<CardGroup>
+        {
+            new()
+            {
+                Count = 1,
+                Cards = new List<Card> { Cards.All.Single(c => c.Rank == Ranks.Ace) }
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.King).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Queen).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Jack).ToList()
+            },
+            new()
+            {
+                Count = 1,
+                Cards = Cards.All.WhereRank(Ranks.Ten).ToList()
+            }
+        }
+    };
+
+    public static List<HandRequirement> All { get; } = new()
+    {
+        SixHigh,
+        SevenHigh,
+        EightHigh,
+        NineHigh,
+        TenHigh,
+        JackHigh,
+        QueenHigh,
+        KingHigh,
+        AceHigh
+    };
+}
